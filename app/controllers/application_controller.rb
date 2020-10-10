e88c710a72183049b14ffe6902ad0ab124e586ea
Andrey Groza <grozwalker@gmail.com>
2020-10-11 00:42:48 +0300

    added login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  include Authentification
+
+  helper_method :logged_in?, :current_user
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
 
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
+      log_in user
 
+      redirect_to user
     else
       flash.now[:danger] = 'Login/password incorrect'
       render 'new'
