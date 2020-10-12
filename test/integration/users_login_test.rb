d4d0dca9121501644deac8bf00bcb4d864359a01
Andrey Groza <grozwalker@gmail.com>
2020-10-13 01:21:12 +0300

    added user login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,12 +3,17 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users :andrey
+  end
+
   test 'login with invalid data' do
     get login_path
     assert_template 'sessions/new'
 
     params = { session: { email: 'test@test.ru', password: 'invalid ' } }
     post login_path, params: params
+
     assert_template 'sessions/new'
 
     assert_not flash.empty?
@@ -16,5 +21,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
 
     assert flash.empty?
+  end
+
+  test 'login with valid data' do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'test' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
   end
 end
