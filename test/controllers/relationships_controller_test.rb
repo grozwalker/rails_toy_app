9da249f0cfe6f167200ac2118cddc445dd68ec9c
Andrey Groza <grozwalker@gmail.com>
2021-01-10 23:41:04 +0300

    add relationship test

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -10,11 +10,34 @@ class RelationshipsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to login_url
   end
+
   test 'guest cant destroy' do
     assert_no_difference 'Relationship.count' do
       post relationships_url
     end
 
     assert_redirected_to login_url
+  end
+
+  test 'create' do
+    user = users :ivan
+    other = users :igor
+
+    log_in_as user
+
+    assert_difference 'user.following.count', 1 do
+      post relationships_url, params: { followed_id: other.id }
+    end
+  end
+
+  test 'destroy' do
+    relationship = relationships :kurb_zaur
+    user = relationship.follower
+
+    log_in_as user
+
+    assert_difference 'user.following.count', -1 do
+      delete relationship_url(relationship)
+    end
   end
 end
